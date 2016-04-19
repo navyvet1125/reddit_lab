2faa3e69347d2459a50f4e6781a62d097c6cfc9b
Evan Washington <enavy04@gmail.com>
2016-04-19 12:01:49 -0700

    Added more comments

--- routes/reddit.js
+++ routes/reddit.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var request = require('request');
 var router = express.Router();
+//Used to store all of our entries
 var entries =[];
+//Change the following number to change how many entries are displayed
 var numberOfEntries = 10;
 
 router.get('/', function(req,res){
+	//Empty string
 	var message = '';
 	for(var i = 0; i< entries.length; i++){
+		//Add each individual entry to the message string
 		message += '<img src="' + entries[i].myImage + '"/><br/><h1>This post has a score of ' + entries[i].score + ' and ' + entries[i].numComments +' comments</h1><hr>';
 	}
+	//Send the large message string
 	res.send(message);
 });
 
@@ -18,13 +23,15 @@ request('http://reddit.com/r/funny.json', function(err, response, body){
 	}
 
 	// convert JSON to a JS object
-
 	var myResult = JSON.parse(body);
+
 	//drill in to get specific pieces of data
 	for(var i = 0; i<numberOfEntries; i++){
+
 		//Create a new object to store the data
-		
 		var singleEntry ={};
+
+		//Load the object with information
 		singleEntry.score = myResult.data.children[i].data.score;
 		singleEntry.numComments = myResult.data.children[i].data.num_comments;
 		singleEntry.myImage = myResult.data.children[i].data.thumbnail;
